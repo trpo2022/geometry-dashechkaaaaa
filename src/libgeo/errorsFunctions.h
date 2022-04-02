d154042bd3e983ce8bab831b6ad864da0b872d91
Daria Lenshina <tvoyachaechka@yandex.ru>
2022-04-02 19:32:43 +0700

    Putting error output into a separate function

---
+++ src/libgeo/errorsFunctions.h
@@ -0,0 +1,5 @@
+#pragma once
+
+char* parseCircle(char*, char*, char*, double*, double*, double*);
+char* parseTriangle(char*, char*, char*, double*, double*, double*, double*, double*, double*, double*, double*);
+void validTriangle(double a, double b, double c);
